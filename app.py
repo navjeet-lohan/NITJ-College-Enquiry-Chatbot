12ad7cf1ddf27eca91e6d1ae2ac305a6ed30e76e
Navjeet Lohan <navjeetlohan@gmail.com>
2025-03-24 15:14:17 +0530

    Chatbot 1.0

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from chat import get_response
 
-import os  # Import the os module to access environment variables
-
 app = Flask(__name__)
-CORS(app)
 
 @app.get("/")
 def index_get():
@@ -13,11 +10,10 @@ def index_get():
 @app.post("/predict")
 def predict():
     text = request.get_json().get("message")
-    response = get_response(text)
-    message = {"answer": response}
+    # TODO: check if text is valid
+    response  =  get_response(text)
+    message   = {"answer"  : response}
     return jsonify(message)
 
 if __name__ == "__main__":
-    # Use the PORT environment variable provided by Render
-    port = int(os.environ.get("PORT", 10000))  # Default to 10000 if PORT is not set
-    app.run(host="0.0.0.0", port=port)  # Bind to 0.0.0.0
+    app.run(debug=True)
